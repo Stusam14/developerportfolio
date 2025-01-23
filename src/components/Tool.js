0ac0bde79d6fbef00266633bef794abd1ac17828
Siyabonga <sngsiy006@myuct.ac.za>
2025-01-23 03:26:47 +0200

    Added two Components , Tools and Tool ,

---
+++ src/components/Tool.js
@@ -0,0 +1,21 @@
+
+const Tool = ({title, noYearExpereince}) => {
+
+    return (
+        <div style={{ }}>
+            <h1 style={{
+                width: "124px",
+                height: "56px",
+                fontSize: "48px",
+
+            }}
+        >
+                {title}
+            </h1>
+            <p>
+                {noYearExpereince}
+            </p>
+
+        </div>
+    )
+}
---
+++ src/components/Tools.js
@@ -0,0 +1,36 @@
+
+const Tools = ()=>{
+
+
+    const toolData = [
+        {
+            title: "HTML",
+            noYearExperience: "4 Years Experience",
+        },
+        {
+            title: "CSS",
+            noYearExperience: "4 Years Experience",
+        },
+        {
+            title: "JavaScript",
+            noYearExperience: "4 Years Experience",
+        },
+        {
+            title: "Accessibility",
+            noYearExperience: "4 Years Experience",
+        },
+        {
+            title: "React",
+            noYearExperience: "3 Years Experience",
+        },
+        {
+            title: "Sass",
+            noYearExperience: "3 Years Experience",
+        }
+    ];
+    return (
+        <div>
+            
+        </div>
+    )
+}
