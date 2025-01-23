48f09566177fe5e62d01cf41e342a4e3e27b6505
Siyabonga <sngsiy006@myuct.ac.za>
2025-01-23 03:27:22 +0200

    Added components for Tools on each file , and Portfolio,

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Portfolio from "./components/Portfolio";
+
+const App = () => {
+
+  return (
+    <div style={{
+      backgroundColor: "grey",
+      maxWidth: "1110px",
+      margin: "0 auto",  
+      
+      }}
+    >
+      <Portfolio/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,22 @@
+import Home from "./Home";
+
+const Portfolio = () => {
+
+    return (
+    <>
+        <div style={{
+            backgroundColor: "blue" ,  
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            }}
+        >
+            <Home />
+        </div>
+        <footer>
+            <div></div>
+        </footer>
+    </>)
+}
+
+export default Portfolio;
