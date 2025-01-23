efff79093ee03c17fc377e13027ae84b1be0a35f
Siyabonga <sngsiy006@myuct.ac.za>
2025-01-23 05:04:14 +0200

    Added images,

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,27 @@ const Home = ()=>{
 
     return (
         <div>
-            <h1>adamkeyes</h1>
-            <Homecard/>
-            <button style={{width: "117px", 
-                border: "none",
-                color: "white", 
-                height: "26px", 
-                textDecoration: "underline",
-                textDecorationThickness: "2px",
-                textDecorationColor: "#4EE1A0",
-                textUnderlineOffset: "5px",
-                backgroundColor: "#151515",
+            <div style={{color:"blue",boxSizing: "content-box", display: "flex"}}>
 
-                }}
-            >CONTACT ME</button>
+                <div>
+                    <h1>adamkeyes</h1>
+                    <button style={{width: "117px", 
+                        border: "none",
+                        color: "white", 
+                        height: "26px", 
+                        textDecoration: "underline",
+                        textDecorationThickness: "2px",
+                        textDecorationColor: "#4EE1A0",
+                        textUnderlineOffset: "10px",
+                        backgroundColor: "#151515",
+
+                        }}
+                    >CONTACT ME</button>
+                </div>
+                <img alt="ImgProfile" src="./images/image-profile-desktop.webp" style={{ maxWidth: "445px", maxHeight: "720px",}}/>
+            </div>
+            <Homecard/>
+            
         </div>
     )
 }
